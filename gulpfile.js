ac29d147ef2b9d4e9dfadff1aa777246d6e8b7ab
khodor <khodor.ammar@gmail.com>
2019-01-14 21:25:06 +0400

    update gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,75 +1,87 @@
-const gulp = require('gulp');
-const sass = require('gulp-sass');
-const postcss = require('gulp-postcss');
-const autoprefixer = require('autoprefixer');
-const cssnano = require('cssnano');
-const browserSync = require('browser-sync').create();
+const gulp = require("gulp");
+const sass = require("gulp-sass");
+const postcss = require("gulp-postcss");
+const autoprefixer = require("autoprefixer");
+const cssnano = require("cssnano");
+const browserSync = require("browser-sync").create();
 const path = require("path");
-const access = require('gulp-accessibility');
-const w3cjs = require('gulp-w3cjs');
+const access = require("gulp-accessibility");
+const w3cjs = require("gulp-w3cjs");
 
-gulp.task('sass', function() {
-    return gulp.src(['assets/sass/style.scss'])
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('assets/sass/'));
-});
+const sassTask = function() {
+  return gulp
+    .src(["assets/sass/style.scss"])
+    .pipe(sass().on("error", sass.logError))
+    .pipe(gulp.dest("assets/sass/"));
+};
 
-gulp.task('postcss:dev', ['sass'], function() {
-    var processors = [
-        autoprefixer({
-            browsers: ['last 2 versions']
-        })
-    ];
+const postcssDev = function() {
+  var processors = [
+    autoprefixer({
+      browsers: ["last 2 versions"]
+    })
+  ];
 
-    return gulp.src('assets/css/*.css')
-        .pipe(postcss(processors))
-        .pipe(gulp.dest('assets/css/'))
-        .pipe(browserSync.stream({
-            match: "**/*.css"
-        }));
-});
+  return gulp
+    .src("assets/css/*.css")
+    .pipe(postcss(processors))
+    .pipe(gulp.dest("assets/css/"))
+    .pipe(
+      browserSync.stream({
+        match: "**/*.css"
+      })
+    );
+};
 
-gulp.task('postcss:prod', ['sass'], function() {
-    var processors = [
-        autoprefixer({
-            browsers: ['last 2 versions']
-        }),
-        cssnano({
-            safe: true
-        })
-    ];
+const postcssProd = function() {
+  var processors = [
+    autoprefixer({
+      browsers: ["last 2 versions"]
+    }),
+    cssnano({
+      safe: true
+    })
+  ];
 
-    return gulp.src('assets/sass/*.css')
-        .pipe(postcss(processors))
-        .pipe(gulp.dest('assets/sass/'));
-});
+  return gulp
+    .src("assets/sass/*.css")
+    .pipe(postcss(processors))
+    .pipe(gulp.dest("assets/sass/"));
+};
 
-gulp.task('watch', function() {
-    gulp.watch(['assets/sass/**/*.scss'], ['postcss:dev']);
-});
+const watch = function() {
+  return gulp.watch(["assets/sass/**/*.scss"], gulp.series(sass, postcssDev));
+};
 
-gulp.task('browserSync', function() {
-    browserSync.init({
-        server: {
-            baseDir: "./"
-        }
-    });
-});
+const browserSyncTask = function(done) {
+  browserSync.init({
+    server: {
+      baseDir: "./"
+    }
+  });
+  done();
+};
 
-gulp.task('w3cjs', function() {
-    return gulp.src('index.html')
-        .pipe(w3cjs())
-        .pipe(w3cjs.reporter());
-});
+const w3cjsTask = function() {
+  return gulp
+    .src("index.html")
+    .pipe(w3cjs())
+    .pipe(w3cjs.reporter());
+};
 
+const a11yTask = function() {
+  return gulp
+    .src("index.html")
+    .pipe(
+      access({
+        force: true
+      })
+    )
+    .on("error", console.log);
+};
 
-gulp.task('a11y', function() {
-    return gulp.src('index.html')
-        .pipe(access({
-            force: true
-        }))
-        .on('error', console.log);
-});
+const build = gulp.series(sassTask, postcssDev, browserSyncTask, watch);
+const prod = gulp.series(sassTask, postcssProd);
 
-gulp.task('default', ['postcss:dev', 'browserSync', 'watch']);
-gulp.task('prod', ['postcss:prod']);
+exports.default = build;
+exports.prod = prod;
